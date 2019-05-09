39517668598d99e19da44ecae84fbbf89ccd08b0
Filip <koudas1009@gmail.com>
2019-05-09 04:04:11 +0200

    automaticDeploy

---
+++ docs/docxToMd.ps1
@@ -0,0 +1,19 @@
+$outputDir = '../_posts';
+$processedPath = './processed';
+$date = Get-Date -UFormat "%Y-%m-%d";
+
+Get-ChildItem .\queue\ | ForEach-Object { 
+    $name = $_.Name.Trim('.docx');
+    $outputName = $date + '-' + $name + '.md';
+    if (Test-Path $outputDir + '/' + $outputName) {
+        continue;
+    }
+
+    echo $outputName
+    echo $outputDir + '/' + $outputName
+
+    pandoc -s $_.FullName -t gfm -o $outputName 
+    Move-Item $_.FullName $processedPath
+    Move-Item $outputName $outputDir
+}
+echo 'DONE'
