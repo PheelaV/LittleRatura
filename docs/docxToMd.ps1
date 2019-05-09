9002ac7b5b4e015f36c943dbed68363b91811e33
Filip <koudas1009@gmail.com>
2019-05-10 01:40:41 +0200

    generation of frontMatter header

--- docs/docxToMd.ps1
+++ docs/docxToMd.ps1
@@ -1,18 +1,28 @@
-$outputDir = '../_posts';
-$processedPath = './processed';
+$docsFolder = "docs"
+$postOutput = '../_posts';
+$docxOutput = $docsFolder + '/processed';
 $date = Get-Date -UFormat "%Y-%m-%d";
 
-Get-ChildItem .\queue\ | ForEach-Object { 
+Get-ChildItem ./queue/ | ForEach-Object { 
     $name = $_.Name.Trim('.docx');
-    $outputName = $date + '-' + $name + '.md';
-    if (Test-Path $($outputDir + '/' + $outputName)) {
-        continue;
-    }
+    $postName = $date + '-' + $name + '.md';
+    $pageDownload = '/' + $docxOutput + '/' + $_.Name
+    Write-Output $postName
 
-    Write-Output $outputName
-
-    pandoc -s $_.FullName -t gfm -o $outputName 
-    Move-Item $_.FullName $processedPath
-    Move-Item $outputName $outputDir
+    pandoc -s $_.FullName -t gfm -o $postName 
+    $frontMatter = @(
+        "---",
+        "#see https://github.com/Feelav/LittleRatura/blob/master/README.md Posting new books",
+        "title: title",
+        "layout: post",
+        "tags: []",
+        $("pageDownload: " + $pageDownload),
+        "---",
+        "")
+    $frontMatter + (Get-Content $postName) | Set-Content $postName
+   
+    Move-Item $_.FullName $('../' + $docxOutput) 
+    Move-Item $postName $postOutput
+    
 }
 Write-Output 'DONE'
