c3fb71e56a8d989507ce8307ca88c241b54aa361
Filip <koudas1009@gmail.com>
2019-05-13 17:35:18 +0200

    FrontMatter handling support

--- docs/docxToMd.ps1
+++ docs/docxToMd.ps1
@@ -1,26 +1,43 @@
 $docsFolder = "docs"
 $postOutput = '../_posts';
 $docxOutput = $docsFolder + '/processed';
+$queueFolder = './queue'
 $date = Get-Date -UFormat "%Y-%m-%d";
 
-Get-ChildItem ./queue/ | ForEach-Object { 
-    $name = $_.Name.Trim('.docx');
-    $postName = $date + '-' + $name + '.md';
+
+Get-ChildItem $queueFolder | ForEach-Object { 
+    $frontMatter = @();
+
+    if($_.Extension -eq '.yml'){
+        continue
+    }
+
+    $rawName = $_.Name.Trim('.docx');
+    $postName = $date + '-' + $rawName + '.md';
     $pageDownload = '/' + $docxOutput + '/' + $_.Name
+    $yamlFileName = $rawName + '.yml'
+    $yamlFileFullName = $queueFolder + '/' + $yamlFileName
     Write-Output $postName
 
     pandoc -s $_.FullName -t gfm -o $postName 
-    $frontMatter = @(
-        "---",
-        "#see https://github.com/Feelav/LittleRatura/blob/master/README.md Posting new books",
-        "title: title",
-        "categories: [SV-CZ-18]",
-        "layout: post",
-        "tags: []",
-        $("pageDownload: " + $pageDownload),
-        "description: | čtenářský deník",
-        "---",
-        "")
+    if(Test-Path $yamlFileFullName){
+        $frontMatter = Get-Content $yamlFileFullName
+
+        Move-Item $yamlFileFullName $('../' + $docxOutput) 
+    } else {
+        $frontMatter = @(
+            "---",
+            "#see https://github.com/Feelav/LittleRatura/blob/master/README.md Posting new books",
+            "title: ",
+            "categories: []",
+            "layout: post",
+            "tags: []",
+            $("pageDownload: " + $pageDownload),
+            "description: | čtenářský deník",
+            "---",
+            "")
+    }
+
     $frontMatter + (Get-Content $postName) | Set-Content $postName
    
     Move-Item $_.FullName $('../' + $docxOutput) 
